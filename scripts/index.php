bb91a82905858c38b2bed8352b8a5ae80f9ac65b
Noah Rousseau <noah.rousseau2412@gmail.com>
2023-10-30 18:41:49 +0100

    push

--- scripts/index.php
+++ scripts/index.php
@@ -7,7 +7,8 @@ include_once('config.php');
 <!DOCTYPE html>
 <html lang='en'>
     <head>
-        
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4682722806417004"
+    		 crossorigin="anonymous"></script>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="/style.css">
--- scripts/login.php
+++ scripts/login.php
@@ -6,7 +6,8 @@ include 'sessionChecks.php';
 <!DOCTYPE html>
 <html lang='en'>
 	<head>
-		
+		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4682722806417004"
+    		 crossorigin="anonymous"></script>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="/style.css">
