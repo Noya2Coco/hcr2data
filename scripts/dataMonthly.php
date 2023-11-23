41cf82e0778d7e94cd7cd1155b5a99891d1afbc1
noah.rousseau2412@gmail.com <noah.rousseau2412@gmail.com>
2023-11-23 23:34:50 +0100

    première tentative regler espace visible table

--- scripts/dataMonthly.php
+++ scripts/dataMonthly.php
@@ -6,106 +6,103 @@ include_once('config.php');
 <!DOCTYPE html>
 <html lang='en'>
     <head>
-			 
+
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="/style.css">
-        
+
         <title>Hcr2 Data</title>
         <link rel="icon" href="/blackDatabase.ico" type="image/x-icon">
         <script>
-            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
+            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
                 var favicon = document.querySelector("link[rel='icon']");
                 favicon.href = "/whiteDatabase.ico";
             }
         </script>
-		
-		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4682722806417004"
-    		 crossorigin="anonymous">
-		</script>
-		
+
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4682722806417004"
+                crossorigin="anonymous">
+        </script>
+
     </head>
-    
+
     <body>
         <?php
         include 'navbar.php';
-	
-		$folderPath = '../../data/dataMonthly';
-		$files = scandir($folderPath);
-		
-		?>
-		
-		<div class="showTable">
-			<div class="table-container">
-					<p>
-						<?php
-						echo $lang['select_month'];
-						?>
-					</p>
-				<table>
-					<?php
-					$chosenPath = "/chosenDataMonthly";
-					
-					$nameMonths = ['january', 'february', 'march', 'april', 'may', 'june', 
-						'july', 'august', 'september', 'october', 'november', 'december'];
-					$months = array('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12');
-					
-					// Créez une nouvelle liste de noms de fichiers sans extension
-					$filesName = [];
-					foreach ($files as $fileName) {
-						// Utilisez la fonction pathinfo pour extraire le nom de fichier sans extension
-						$filesName[] = pathinfo($fileName, PATHINFO_FILENAME);
-					}
-					
-					// Obtenez les années uniques à partir des noms de fichiers
-					$years = array();
-					foreach ($filesName as $fileName) {
-						if (strpos($fileName, '-') !== false) {
-							list($year, $month) = explode('-', $fileName);
-							$years[$year] = true;
-						}
-					}
-					
 
-					// Créez dynamiquement les entrées du tableau pour chaque année et mois
-					foreach (array_keys($years) as $year) {
-						foreach ($months as $m) {
-							$table[$year][$m] = '•';
-						}
-					}
+        $folderPath = '../../data/dataMonthly';
+        $files = scandir($folderPath);
+        ?>
 
-					// Placez les noms de fichiers existants dans le table
-					foreach ($filesName as $fileName) {
-						if (strpos($fileName, '-') !== false) {
-							list($year, $month) = explode('-', $fileName);
-							$table[$year][$month] = $fileName;
-						}
-					}
+        <div class="showTable">
+            <div class="table-container">
+                <p>
+                    <?php
+                    echo $lang['select_month'];
+                    ?>
+                </p>
+                <table>
+                    <?php
+                    $chosenPath = "/chosenDataMonthly";
 
-					// Générez le table HTML
-					echo '<tr><td class="table-title">' . $lang['month_year'] . '</td>';
-					foreach (array_keys($table) as $year) {
-						echo '<td class="table-title">20' . $year . '</td>';
-					}
-					echo '</tr>';
-					foreach ($months as $m) {
-						echo '<tr><td class="table-title">' . $lang[$nameMonths[intval($m) -1]] . '</td>';
-						foreach (array_keys($table) as $year) {
-							if ($table[$year][$m] === '•'){
-								echo '<td><a class="any-data">' . $table[$year][$m] . '</a></td>';
-							}
-							else{
-								echo '<td><a href=' . $chosenPath . '?month=' . $table[$year][$m] . '>' . $table[$year][$m] . '</a></td>';
-							}
-						}
-						echo '</tr>';
-					}
-					
-				?>
-				</table>
-			</div>
-		</div>
-		<script src="../checkScrollbar.js"></script>
+                    $nameMonths = ['january', 'february', 'march', 'april', 'may', 'june',
+                        'july', 'august', 'september', 'october', 'november', 'december'];
+                    $months = array('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12');
 
-	</body>
+                    // Créez une nouvelle liste de noms de fichiers sans extension
+                    $filesName = [];
+                    foreach ($files as $fileName) {
+                        // Utilisez la fonction pathinfo pour extraire le nom de fichier sans extension
+                        $filesName[] = pathinfo($fileName, PATHINFO_FILENAME);
+                    }
+
+                    // Obtenez les années uniques à partir des noms de fichiers
+                    $years = array();
+                    foreach ($filesName as $fileName) {
+                        if (strpos($fileName, '-') !== false) {
+                            list($year, $month) = explode('-', $fileName);
+                            $years[$year] = true;
+                        }
+                    }
+
+
+                    // Créez dynamiquement les entrées du tableau pour chaque année et mois
+                    foreach (array_keys($years) as $year) {
+                        foreach ($months as $m) {
+                            $table[$year][$m] = '•';
+                        }
+                    }
+
+                    // Placez les noms de fichiers existants dans le table
+                    foreach ($filesName as $fileName) {
+                        if (strpos($fileName, '-') !== false) {
+                            list($year, $month) = explode('-', $fileName);
+                            $table[$year][$month] = $fileName;
+                        }
+                    }
+
+                    // Générez le table HTML
+                    echo '<tr><td class="table-title">' . $lang['month_year'] . '</td>';
+                    foreach (array_keys($table) as $year) {
+                        echo '<td class="table-title">20' . $year . '</td>';
+                    }
+                    echo '</tr>';
+                    foreach ($months as $m) {
+                        echo '<tr><td class="table-title">' . $lang[$nameMonths[intval($m) - 1]] . '</td>';
+                        foreach (array_keys($table) as $year) {
+                            if ($table[$year][$m] === '•') {
+                                echo '<td><a class="any-data">' . $table[$year][$m] . '</a></td>';
+                            } else {
+                                echo '<td><a href=' . $chosenPath . '?month=' . $table[$year][$m] . '>' . $table[$year][$m] . '</a></td>';
+                            }
+                        }
+                        echo '</tr>';
+                    }
+                    ?>
+                </table>
+            </div>
+        </div>
+        <script src="../checkScrollbar.js"></script>
+
+    </body>
 </html>
--- scripts/showData.php
+++ scripts/showData.php
@@ -1,5 +1,5 @@
 <?php
-	include_once('config.php');
+include_once('config.php');
 ?>
 
 <!DOCTYPE html>
@@ -9,108 +9,98 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="/style.css">
-        
+
         <title>Hcr2 Data</title>
         <link rel="icon" href="/blackDatabase.ico" type="image/x-icon">
         <script>
-            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
+            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
                 var favicon = document.querySelector("link[rel='icon']");
                 favicon.href = "/whiteDatabase.ico";
             }
         </script>
-		
-		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4682722806417004"
-    		 crossorigin="anonymous">
-		</script>
-		
+
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4682722806417004"
+                crossorigin="anonymous">
+        </script>
+
     </head>
-	
-	<body>
-		<?php
-		include 'navbar.php';
 
-		$monthList = ["january", "february", "march", "april", "may", "june",
-						"july", "august", "september", "october", "november", "december"];
+    <body>
+        <?php
+        include 'navbar.php';
 
-		?>	
-		
-		<div class="showGraph">
-			<div class="graph-container">
-				<?php
-					echo '<p>' . $lang[$monthList[(int)substr($folderAndFilePath[1], 3, 2) -1]] 
-						. ' 20' . substr($folderAndFilePath[1], 0, 2) . '</p>';
-					echo '<img src=/includes/graphRedirect.php?image=' . pathinfo($folderAndFilePath[1])['filename'] . '.png alt="Graph month data">';
-				?>
-			</div>
-		</div>
+        $monthList = ["january", "february", "march", "april", "may", "june",
+            "july", "august", "september", "october", "november", "december"];
+        ?>	
 
-		<?php
-		require '../../vendor/autoload.php'; // Inclure l'autoloader de Composer
+        <div class="showGraph">
+            <div class="graph-container">
+                <?php
+                echo '<p>' . $lang[$monthList[(int) substr($folderAndFilePath[1], 3, 2) - 1]]
+                . ' 20' . substr($folderAndFilePath[1], 0, 2) . '</p>';
+                echo '<img src=/includes/graphRedirect.php?image=' . pathinfo($folderAndFilePath[1])['filename'] . '.png alt="Graph month data">';
+                ?>
+            </div>
+        </div>
 
-		use PhpOffice\PhpSpreadsheet\IOFactory;
+        <?php
+        require '../../vendor/autoload.php'; // Inclure l'autoloader de Composer
 
-		// Chemin vers le fichier Excel
-		$chemin_fichier = '../../data/' . $folderAndFilePath[0] . $folderAndFilePath[1];
-		
-		try
-		{
-			// Charger le fichier Excel
-			$spreadsheet = IOFactory::load($chemin_fichier);
+        use PhpOffice\PhpSpreadsheet\IOFactory;
 
-			// Sélectionner la première feuille de calcul
-			$worksheet = $spreadsheet->getActiveSheet();
+// Chemin vers le fichier Excel
+        $chemin_fichier = '../../data/' . $folderAndFilePath[0] . $folderAndFilePath[1];
 
-			// Récupérer les données de la feuille de calcul
-			$donnees = $worksheet->toArray();
-			
-			?>
-			<div class="showTable">
-				<div class="table-container">
-					<?php
-					echo '<p>' . $lang[$monthList[(int)substr($folderAndFilePath[1], 3, 2) -1]]
-						. ' 20' . substr($folderAndFilePath[1], 0, 2) . '</p>'
-						. '<table>';
+        try {
+            // Charger le fichier Excel
+            $spreadsheet = IOFactory::load($chemin_fichier);
 
-			// Boucle pour parcourir les lignes du tableau
-			foreach ($donnees as $rowIndex => $row) {
-				echo '<tr>';
-				$numColumns = 1;
-				
-				// Boucle pour parcourir les cellules de chaque ligne
-				foreach ($row as $cellValue) {
-					$class = '';
+            // Sélectionner la première feuille de calcul
+            $worksheet = $spreadsheet->getActiveSheet();
 
-					// Appliquer la classe 'table-title' à la colonne A et à la ligne 1
-					if ($rowIndex === 0 || $numColumns == 1){
-						$class = 'table-title';
-						echo '<td class="' . $class . ' column-' . $numColumns . '">' . $cellValue . '</td>';
-					}
-					
-					elseif ($rowIndex === 1 && $numColumns != 1){
-						$class = 'table-date';
-						echo '<td class="' . $class . ' column-' . $numColumns . '">' . $cellValue . '</td>';
-					}
-					else {
-						echo '<td class="' . $class . ' column-' . $numColumns . '">' . $cellValue . '</td>';
-					}
-					
-					$numColumns += 1;
-				}
+            // Récupérer les données de la feuille de calcul
+            $donnees = $worksheet->toArray();
+            ?>
+            <div class="showTable">
+                <div class="table-container">
+            <?php
+            echo '<p>' . $lang[$monthList[(int) substr($folderAndFilePath[1], 3, 2) - 1]]
+            . ' 20' . substr($folderAndFilePath[1], 0, 2) . '</p>'
+            . '<table>';
 
-				echo '</tr>';
-			}
+            // Boucle pour parcourir les lignes du tableau
+            foreach ($donnees as $rowIndex => $row) {
+                echo '<tr>';
+                $numColumns = 1;
 
-			?>
-					</table>
-				</div>
-			</div>
-			
-			<script src="../checkScrollbar.js"></script>
-			<?php
-		} 
+                // Boucle pour parcourir les cellules de chaque ligne
+                foreach ($row as $cellValue) {
+                    $class = '';
 
-		catch (Exception $e) 
-		{
-			echo 'Une erreur s\'est produite : ', $e->getMessage();
-		}
-		?>
+                    // Appliquer la classe 'table-title' à la colonne A et à la ligne 1
+                    if ($rowIndex === 0 || $numColumns == 1) {
+                        $class = 'table-title';
+                        echo '<td class="' . $class . ' column-' . $numColumns . '">' . $cellValue . '</td>';
+                    } elseif ($rowIndex === 1 && $numColumns != 1) {
+                        $class = 'table-date';
+                        echo '<td class="' . $class . ' column-' . $numColumns . '">' . $cellValue . '</td>';
+                    } else {
+                        echo '<td class="' . $class . ' column-' . $numColumns . '">' . $cellValue . '</td>';
+                    }
+
+                    $numColumns += 1;
+                }
+
+                echo '</tr>';
+            }
+            ?>
+                    </table>
+                    </div>
+            </div>
+
+            <script src="../checkScrollbar.js"></script>
+                    <?php
+                } catch (Exception $e) {
+                    echo 'Une erreur s\'est produite : ', $e->getMessage();
+                }
+                ?>
